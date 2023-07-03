6ebe919e8c12c559beabedb9c4d8b4689a87818c
Sarah West <west0883@umn.edu>
2023-07-03 15:42:18 -0500

    initial commit

---
+++ pipeline_gait_analysis.m
@@ -0,0 +1,7 @@
+% pipeline_gait_analysis.m
+% Sarah West
+% 7/3/23
+
+% Uses extracted and behavior-segmented traces of paw/body part velocities from
+% DeepLabCut to find locomotion strides. Then uses those strides to run
+% gait analysis.
