e565cba3433b44a931da75d9a62576be5d81ff9b
Sarah West <west0883@umn.edu>
2023-07-19 11:09:01 -0500

    calls SegmentTimeseriesData for different instances of strides

---
+++ StrideSegmentationLooper.m
@@ -0,0 +1,58 @@
+% StrideSegmentationLooper.m
+% Sarah West
+% 7/19/23
+
+function [parameters] = StrideSegmentationLooper(parameters)
+
+    % Inputs 
+    % parameters.timeseries -- 
+    % parameters.time_ranges -- 
+
+    MessageToUser('Segmenting ', parameters);
+
+    timeseries = parameters.timeseries;
+    time_ranges = parameters.time_ranges;
+
+    subParameters.segmentDim = parameters.segmentDim;
+    subParameters.concatDim = parameters.concatDim;
+    subParameters.uniformSegments = parameters.uniformSegments;
+
+    % If true, is continued walk long periods
+    if parameters.instancesAsCells
+
+        segmentations_all = cell(size(timeseries));
+
+        for datai = 1:numel(timeseries)
+            
+            timeseries_holder = timeseries{datai}; 
+            time_ranges_holder = vertcat(time_ranges{datai}{:});
+
+            subParameters.timeseries = timeseries_holder;
+            subParameters.time_ranges = time_ranges_holder;
+
+            subParameters = SegmentTimeseriesData(subParameters);
+
+            segmentations_all{datai} = subParameters.segmented_timeseries;
+
+        end
+        
+        % add another layer of cells to match other periods formatting
+        segmentations_all = {segmentations_all};
+  
+    % Otherwise, is all other periods
+    else
+        timeseries_holder = timeseries;
+        time_ranges_holder = time_ranges; 
+
+        subParameters.timeseries = timeseries_holder;
+        subParameters.time_ranges = time_ranges_holder;
+        
+        subParameters = SegmentTimeseriesData(subParameters);
+
+        segmentations_all = subParameters.segmented_timeseries;
+    end 
+    
+    % Put into output structure
+    parameters.segmented_timeseries = segmentations_all;
+    
+end 
